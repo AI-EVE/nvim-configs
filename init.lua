0f06f89d89aee5cd70366e48428ddb04a24ce0e2
AI-EVE <hussampc50@gmail.com>
2025-01-17 05:04:45 +0000

    adding commint.nvim

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set rnu")
 vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>")
