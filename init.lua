8771d106862f14290463d1635f812a18eeb2accf
AI-EVE <hussampc50@gmail.com>
2025-01-17 03:52:18 +0000

    second

--- init.lua
+++ init.lua
@@ -3,8 +3,4 @@ vim.cmd("set tabstop=2")
 vim.cmd("set rnu")
 vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<leader>ho", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, {})
 require("config.lazy")
--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -1,30 +1,34 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_install = {
-				"black",
-				"mypy",
-				"ruff",
-			},
-		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "ts_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({capabilities = capabilities})
-			lspconfig.pyright.setup({capabilities = capabilities})
-			lspconfig.ts_ls.setup({capabilities = capabilities})
-		end,
-	},
+        {
+                "williamboman/mason.nvim",
+                opts = {
+                        ensure_install = {
+                                "black",
+                                "mypy",
+                                "ruff",
+                        },
+                },
+        },
+        {
+                "williamboman/mason-lspconfig.nvim",
+                config = function()
+                        require("mason-lspconfig").setup({
+                                ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+                        })
+                end,
+        },
+        {
+                "neovim/nvim-lspconfig",
+                config = function()
+                        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup({ capabilities = capabilities })
+                        lspconfig.pyright.setup({ capabilities = capabilities })
+                        lspconfig.ts_ls.setup({ capabilities = capabilities })
+                        vim.keymap.set("n", "<leader>ho", vim.lsp.buf.hover, {})
+                        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+                        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+                        vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, {})
+                end,
+        },
 }
